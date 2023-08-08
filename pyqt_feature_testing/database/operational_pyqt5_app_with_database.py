8ae8f3328aaabaabea0d890a26ba7145994417f2
Achille Martin <achille.martin95@gmail.com>
2023-08-08 02:05:50 +0100

    Added logging capability to operation pyqt5 database app and gitignored the generated .log files

--- pyqt_feature_testing/database/operational_pyqt5_app_with_database.py
+++ pyqt_feature_testing/database/operational_pyqt5_app_with_database.py
@@ -14,6 +14,25 @@ from PyQt5.QtSql import QSqlDatabase, QSqlTableModel, QSqlQuery
 
 import sys
 import os.path # To manage file paths for cross-platform apps
+import logging as log_tool # The logger for debugging
+
+# Define global variables and objects
+
+# Define path reference: app folder is the one containing this script
+app_folder = os.path.dirname(os.path.realpath(__file__))
+
+# Set logger config and instantiate object
+logger_logging_level = "DEBUG"
+logger_output_file_name = "pyqt5-app.log"
+logger_output_prefix_format = "[%(asctime)s] [%(levelname)s] - %(message)s"
+
+logger = log_tool.getLogger(__name__)
+logger.setLevel(logger_logging_level)
+logger_output_file_path = os.path.join(app_folder, str(logger_output_file_name))
+file_handler = log_tool.FileHandler(logger_output_file_path)
+formatter = log_tool.Formatter(logger_output_prefix_format)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 ## Class definition
 
@@ -21,6 +40,8 @@ import os.path # To manage file paths for cross-platform apps
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        logger.debug("MainWindow::__init__ - Entered method")
 
         self.setWindowTitle("Example simple pyqt5 app")
 
@@ -33,16 +54,20 @@ class MainWindow(QMainWindow):
         self.setMaximumSize(QSize(400, 300))
         self.setCentralWidget(button)
 
+        logger.debug("MainWindow::__init__ - Exited method")
+
     def on_button_clicked(self):
         
         # Diplay debug message and alert message
-        print("Button has been clicked!")
+        logger.debug("MainWindow::on_button_clicked - Entered method")
+        logger.info("MainWindow::on_button_clicked - Button has been clicked")
         alert = QMessageBox()
         alert.setText('You clicked the button!\n\nThis will open a database viewer and modifier...')
+        logger.debug("MainWindow::on_button_clicked - Alert message started")
         alert.exec()
+        logger.debug("MainWindow::on_button_clicked - Alert message terminated")
         
         # Database viewer and modifier
-        app_folder = os.path.dirname(os.path.realpath(__file__)) # App folder is the one containing this script
         db_manager = DbManager('QSQLITE', 'sportsdatabase.db', app_folder)
         table_model = QSqlTableModel()
         db_manager.initialise_model(table_model)
@@ -64,7 +89,10 @@ class MainWindow(QMainWindow):
 
         dlg.setLayout(layout)
         dlg.setWindowTitle("Database Demo")
+        logger.debug("MainWindow::on_button_clicked - Database dialog started")
         dlg.exec()
+        logger.debug("MainWindow::on_button_clicked - Database dialog terminated")
+        logger.debug("MainWindow::on_button_clicked - Exited method")
 
 # Database Manager class
 class DbManager():
@@ -73,6 +101,8 @@ class DbManager():
     db_connected = None
     
     def __init__(self, db_type, db_name, db_folder):
+        logger.debug("DbManager::__init__ - Entered method")
+
         self.db_type = db_type
         self.db_name = db_name # Include the extension in the name (e.g. `test.db`)
         self.db_folder = db_folder
@@ -83,26 +113,44 @@ class DbManager():
                 DbManager.db_connected = QSqlDatabase.addDatabase(self.db_type)
                 DbManager.db_connected.setDatabaseName(os.path.join(self.db_folder, self.db_name))
             except Exception as error:
-                print("Error: Connection not established {}".format(error))
+                logger.exception("DbManager::__init__ - Error: Connection not established - " + str(error))
             else:
-                print("Connection established for " + str(self.db_name) + " of type " + str(self.db_type) + " at location " + str(os.path.join(self.db_folder, self.db_name)))
+                logger.info("DbManager::__init__ - Connection established for " 
+                        + str(self.db_name) 
+                        + " of type " 
+                        + str(self.db_type) 
+                        + " at location " 
+                        + str(os.path.join(self.db_folder, self.db_name))
+                        )
 
         # Initial database creation
         if not os.path.exists(os.path.join(self.db_folder, self.db_name)):
             self.create_db()
 
-        print("Database Manager for " + str(os.path.join(self.db_folder, self.db_name)) + " instantiated")
+        logger.info("DbManager::__init__ - Database Manager for " 
+                + str(os.path.join(self.db_folder, self.db_name)) 
+                + " instantiated"
+                )
+
+        logger.debug("DbManager::__init__ - Exited method")
 
     def __del__(self):
+        logger.debug("DbManager::__del__ - Entered method")
         self.db_connected.close()
-        print("Database Manager for " + str(os.path.join(self.db_folder, self.db_name)) + " deleted")
+        logger.info("DbManager::__del__ - Database Manager for " 
+                + str(os.path.join(self.db_folder, self.db_name)) 
+                + " deleted"
+                )
+        logger.debug("DbManager::__del__ - Exited method")
 
     def create_db(self):
+        logger.debug("DbManager::create_db - Entered method")
         if not self.db_connected.open():
           msg = QMessageBox()
           msg.setIcon(QMessageBox.Critical)
           msg.setText("Error in Database Creation")
           retval = msg.exec_()
+          logger.exception("DbManager::create_db - Error in Database Creation")
           return False
 
         query = QSqlQuery()
@@ -113,33 +161,46 @@ class DbManager():
         query.exec_("insert into sportsmen values(103, 'Ussain', 'Bolt')")
         query.exec_("insert into sportsmen values(104, 'Sachin', 'Tendulkar')")
         query.exec_("insert into sportsmen values(105, 'Saina', 'Nehwal')")
+        logger.debug("DbManager::create_db - Created table of sportsmen with initial values")
+        logger.debug("DbManager::create_db - Exited method")
         return True
 
     def initialise_model(self, model):
+        logger.debug("DbManager::initialise_model - Entered method")
         model.setTable('sportsmen')
         model.setEditStrategy(QSqlTableModel.OnFieldChange)
         model.select()
         model.setHeaderData(0, Qt.Horizontal, "ID")
         model.setHeaderData(1, Qt.Horizontal, "First name")
         model.setHeaderData(2, Qt.Horizontal, "Last name")
+        logger.debug("DbManager::initialise_model - Exited method")
 
     def create_view(self, title, model):
+        logger.debug("DbManager::create_view - Entered method")
         view = QTableView()
         view.setModel(model)
         view.setWindowTitle(title)
+        logger.debug("DbManager::create_view - Exited method")
         return view
 
     def add_row(self, model):
+        logger.debug("DbManager::add_row - Entered method")
         # print ("Model row number: " + str(model.rowCount()))
         ret = model.insertRows(model.rowCount(), 1)
         # print ("Model insert state: " + str(ret))
+        logger.debug("DbManager::add_row - Exited method")
 
     def find_row(self, i):
+        logger.debug("DbManager::add_row - Entered method")
         self.delrow = i.row()
+        logger.debug("DbManager::add_row - Exited method")
 
 ## Application definition
 
 def main():
+    logger.info("========================\n")
+    logger.debug("main - Entered function and logger instantiated")
+    logger.debug("main - Log output file can be found at: " + str(logger_output_file_path))
 
     # You need one (and only one) QApplication instance per application.
     # Pass in sys.argv to allow command line arguments for your app.
@@ -149,12 +210,15 @@ def main():
     # Create a Qt widget, which will be our window.
     main_window = MainWindow()
     main_window.show()  # IMPORTANT - Windows are hidden by default.
-
+    
     # Start the event loop.
+    logger.info("main - App started")
     app.exec()
-
+    logger.info("main - App terminated")
+    
     # Your application won't reach here until you exit and the event
     # loop has stopped.
+    logger.debug("main - Exited function")
 
 if __name__ == "__main__":    
     # This needs to only define main    
