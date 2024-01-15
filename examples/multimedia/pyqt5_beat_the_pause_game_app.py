5eafb0d7e2e7011dd3e70b2a153ff630e712558b
achillemartin <achille.martin95@gmail.com>
2024-01-15 05:14:38 +0100

    Added MIT licence to multimedia app

--- examples/multimedia/pyqt5_beat_the_pause_game_app.py
+++ examples/multimedia/pyqt5_beat_the_pause_game_app.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
-# Aim: Beat the pause game by capturing the quickly switched image in a video automatically
+# MIT License
+
+# Copyright (c) 2023-2024 Achille MARTIN
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Aim of the app: Beat the pause game by capturing the quickly switched image in a video automatically
 
 # Refresher: the pause game consists in hiding a picture in one frame of a video and
 # let the user try to see it by pausing the video
