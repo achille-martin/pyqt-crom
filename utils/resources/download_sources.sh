422d5a52b2c2ff3c54c16abf2a3d6cbdb1379206
achillemartin <achille.martin95@gmail.com>
2024-04-19 00:32:55 +0200

    Fixed shebang in download_sources script

--- utils/resources/download_sources.sh
+++ utils/resources/download_sources.sh
@@ -1,3 +1,3 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 wget https://www.python.org/ftp/python/3.10.12/Python-3.10.12.tar.xz
 wget https://download.qt.io/archive/qt/5.15/5.15.2/single/qt-everywhere-src-5.15.2.tar.xz
