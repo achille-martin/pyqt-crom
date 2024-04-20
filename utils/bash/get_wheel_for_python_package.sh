c7fc7596ee3132656d987bdf5af848de0ae00596
achillemartin <achille.martin95@gmail.com>
2024-04-20 13:56:47 +0200

    Improved get wheel for python package script by using path variables and clarifying the output

--- utils/bash/get_wheel_for_python_package.sh
+++ utils/bash/get_wheel_for_python_package.sh
@@ -1,8 +1,36 @@
 #!/usr/bin/env bash
 
-# Purpose:
-# Identify wheels for desired non-standard python package
+# MIT License
+
+# Copyright (c) 2023-2024 Achille MARTIN
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+##########################################
+# Purpose
+# -------
+#
+# Identify wheels 
+# for desired non-standard python package
+# with specific package version
 # for the current OS specifications
+##########################################
 
 # Define a custom error handler function
 handle_error() {
@@ -24,19 +52,21 @@ python_package_name=$1
 python_package_version=$2
 echo "Python package deisred: $python_package_name"
 echo "Python package version desired: $python_package_version"
+echo "------"
 
 # Create a temporary folder to download the wheels
-mkdir -p $HOME/Downloads/temp_wheels
+temp_wheels_folder_path="$HOME/Downloads/temp_wheels"
+mkdir -p $temp_wheels_folder_path
 
 # Download the wheels for the current OS specifications
-pip download --only-binary :all: --dest . --no-cache -d $HOME/Downloads/temp_wheels $python_package_name==$python_package_version
+pip download --only-binary :all: --dest . --no-cache -d $temp_wheels_folder_path $python_package_name==$python_package_version
 
 # Get the name of the wheels from the downloaded material
-wheel_name=$(ls $HOME/Downloads/temp_wheels -tp | grep -v /$ | head -1)
+wheel_name=$(ls $temp_wheels_folder_path -tp | grep -v /$ | head -1)
 
 # Possibility to remove the temporary wheel folder,
 # but might look risky from a user perspective.
-# User can manually deleted if needed.
+# User can manually delete the temporary folder if needed.
 
 # Print out the name of the wheels
 echo "------"
