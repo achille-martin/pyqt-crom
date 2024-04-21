7ae7b2b7b782d4da3b96b4848c92dbf7cd7df9cf
achillemartin <achille.martin95@gmail.com>
2024-04-21 18:36:40 +0200

    Added capture of all output stdout and stderr to md log file for comprehensive debugging help

--- utils/bash/get_command_output_help.sh
+++ utils/bash/get_command_output_help.sh
@@ -84,30 +84,30 @@ text_to_add=""
 log_folder="$PYQT_CROM_DIR/log"
 mkdir -p $log_folder
 
-# Change directory to log folder for practicality
-cd $log_folder
-
 # Create file to hold information
-current_date_time=$(date +"%Y_%m_%d-%H_%M_%S")
-log_file="command_output_help_$current_date_time.md"
+current_date_time=$(date +"%Y_%m_%d-%H_%M_%S" |& tee '/dev/null')
+log_file_name="command_output_help_$current_date_time.md"
+log_file_path="$log_folder/$log_file_name"
 
-printf "[INFO] Creating file $log_folder/$log_file to hold information...\n"
+printf "[INFO] Creating file $log_file_path to hold information...\n"
 
-touch $log_file
+touch $log_file_path
 
 printf "[INFO] ...Done.\n"
 
 # Pre-populate the log file with relevant information about the request
 printf "[INFO] Populating file with relevant information about the request...\n"
 
+current_working_directory=$(pwd |& tee '/dev/null')
 text_to_add="# Command output help
 
 ## Request information
 * Command requested: $code_snippet_start $command_to_run $code_snippet_end
+* Current working directory: $code_snippet_start $current_working_directory $code_snippet_end
 * Date and time requested at: $code_snippet_start $current_date_time $code_snippet_end
 * User emitting request: $code_snippet_start $USER $code_snippet_end
 "
-printf "$text_to_add" >> $log_file 2>&1
+printf "$text_to_add" >> $log_file_path 2>&1
 
 printf "[INFO] ...Done.\n"
 
@@ -116,11 +116,11 @@ printf "[INFO] ...Done.\n"
 ## Get information about source OS
 printf "[INFO] Populating file with information about source OS...\n"
 
-os_pretty_name=$(cat "/etc/os-release" | grep "PRETTY_NAME")
+os_pretty_name=$(cat "/etc/os-release" | grep "PRETTY_NAME" |& tee '/dev/null')
 text_to_add="## Source OS information
 * Source OS pretty name: $code_snippet_start $os_pretty_name $code_snippet_end
 "
-printf "$text_to_add" >> $log_file 2>&1
+printf "$text_to_add" >> $log_file_path 2>&1
 
 printf "[INFO] ...Done.\n"
 
@@ -128,10 +128,10 @@ printf "[INFO] ...Done.\n"
 printf "[INFO] Populating file with information about python...\n"
 
 source $PYQT_CROM_DIR/venv/pyqt-crom-venv/bin/activate
-python_version=$(python --version)
-pip_version=$(pip --version)
-pip_packages_installed=$(pip list --local)
-pip_dependency_tree=$(pipdeptree --local)
+python_version=$(python --version |& tee '/dev/null')
+pip_version=$(pip --version |& tee '/dev/null')
+pip_packages_installed=$(pip list --local |& tee '/dev/null')
+pip_dependency_tree=$(pipdeptree --local |& tee '/dev/null')
 
 text_to_add="## Python information
 * Python version: $code_snippet_start $python_version $code_snippet_end
@@ -139,27 +139,58 @@ text_to_add="## Python information
 * Pip packages installed: $code_snippet_start $pip_packages_installed $code_snippet_end
 * Pip dependency tree: $code_snippet_start $pip_dependency_tree $code_snippet_end
 "
-printf "$text_to_add" >> $log_file 2>&1
+printf "$text_to_add" >> $log_file_path 2>&1
 
 printf "[INFO] ...Done.\n"
 
 ## Get information about the general repo
 printf "[INFO] Populating file with information about repo...\n"
 
+git_branch_current=$(git branch --show-current |& tee '/dev/null')
+latest_commit_date=$(git log -1 --format=%cd |& tee '/dev/null')
+environment_variables=$(cat $PYQT_CROM_DIR/utils/bash/setup_path.sh |& tee '/dev/null')
+
+text_to_add="## General repo information
+* Git branch: $code_snippet_start $git_branch_current $code_snippet_end
+* Latest git commit date: $code_snippet_start $latest_commit_date $code_snippet_end
+* Environment variables: $code_snippet_start $environment_variables $code_snippet_end
+"
+printf "$text_to_add" >> $log_file_path 2>&1
+
 printf "[INFO] ...Done.\n"
 
 ## Get information about repo dependencies
 printf "[INFO] Populating file with information about repo dependencies...\n"
+
+external_sources_list=$(ls -l $PYQT_CROM_DIR/utils/resources |& tee '/dev/null')
+
+text_to_add="## Repo dependency information
+* External sources: $code_snippet_start $external_sources_list $code_snippet_end
+"
+printf "$text_to_add" >> $log_file_path 2>&1
 
 printf "[INFO] ...Done.\n"
 
 # Get information about the command input
 printf "[INFO] Populating file with information about command input...\n"
 
+text_to_add="## Command input information
+* Command run: $code_snippet_start $(eval $command_to_run |& tee '/dev/null') $code_snippet_end
+"
+printf "$text_to_add" >> $log_file_path 2>&1
+
 printf "[INFO] ...Done.\n"
 
 # Append extra file content
 printf "[INFO] Populating file with extra input file content...\n"
+
+text_to_add="## Extra file content information
+"
+printf "$text_to_add" >> $log_file_path 2>&1
+for file_path in $file_input_list; do
+    file_content=$(cat $file_path |& tee '/dev/null')
+    printf "* File \`$file_path\`: $code_snippet_start $file_content $code_snippet_end\n" >> $log_file_path 2>&1
+done
 
 printf "[INFO] ...Done.\n"
 
